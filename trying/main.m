ada408eff9143c9e802ae6a3a436b996b748874b
shadae <shadae@umich.edu>
2018-11-25 12:19:02 -0500

    Initial Commit

---
+++ trying/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  trying
+//
+//  Created by Shadae Boakye-Yiadom on 11/25/18.
+//  Copyright © 2018 Shadae Boakye-Yiadom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
